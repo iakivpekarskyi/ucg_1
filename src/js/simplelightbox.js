b3c3527185690ee5de628ecc4a9a404d44654cf9
Anna Kornieieva <109692085+8-kruzhka-8@users.noreply.github.com>
2022-09-30 22:41:31 +0300

    add js

---
+++ src/js/simplelightbox.js
@@ -0,0 +1,3 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const lightbox = new SimpleLightbox('.gallery a', { captionsData:"alt", captionDelay: "300" });
